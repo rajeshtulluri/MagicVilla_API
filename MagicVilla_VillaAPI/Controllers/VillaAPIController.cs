bcec3414daac6fd920d706c6a846484e3e1ef289
RajeshTulluri <rajes@DESKTOP-HGKM4V5>
2023-09-22 17:29:44 -0500

    Created a Get EndPoint

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,8 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+    [Route("api/VillaAPI")]
+    //[Route("api/[controller]")]
+    [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+        [HttpGet("id")]//This is must to know what type of endpoint it is otherwise we will get error
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public ActionResult<VillaDto> GetVillas(int id)
+        { if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
     }
 }
