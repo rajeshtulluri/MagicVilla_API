604ae3afed63140fd53ca4621e357aa9308e0912
RajeshTulluri <rajes@DESKTOP-HGKM4V5>
2023-09-26 13:32:04 -0500

    Added a comment in VillaStore.cs

--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -2,7 +2,7 @@
 
 namespace MagicVilla_VillaAPI.Data
 {
-    public static class VillaStore
+    public static class VillaStore//This is the temporary store
     {
         public static List<VillaDto> villaList=new List<VillaDto>
             {new VillaDto{Id=1, Name="Pool-View" },
